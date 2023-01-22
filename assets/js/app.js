146956282f28fcfc4d28185219d63f3b520fa41b
abidasultananipa <sultanaabida260@gmail.com>
2023-01-22 10:55:10 +0600

    baymax

---
+++ assets/js/app.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+    // h-slider
+    $('#h-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items: 1,
+        dots: false,
+        smartSpeed: 1000,
+        navText : ['PREV', 'NEXT'],
+        responsive:{
+            0:{
+             nav: false,
+            },
+            768:{
+             nav: true,
+            },
+        }
+    })
+
+    // project
+    $('#project-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav: true,
+        dots: false,
+        smartSpeed: 1000,
+        margin: 24,
+        responsive:{
+            0:{
+                items: 1, 
+                nav: false,
+            },
+            768:{
+              items: 2,
+            },
+            1140:{
+                items: 2,
+                center: true,
+            }
+        }
+    })
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots: true,
+        items: 1,
+        smartSpeed: 800,
+    })
+})
